3cfff024770ab5ead64ba3ff06412c1b93d00f43
tvaira <tvaira@free.fr>
2022-05-10 06:42:57 +0200

    Remanie le code

--- Include/CLASSIQUE.inc.php
+++ Include/CLASSIQUE.inc.php
@@ -1,33 +1,28 @@
 
 <?php
-$mysqli = mysqli_connect("localhost", "celian", "password", "FEUX");
-$requete = "SELECT * FROM CLASSIQUE";
-$resultat = mysqli_query($mysqli, $requete);
 
-                while ($ligne = mysqli_fetch_assoc($resultat))
-
-                {
-
-
-
-                        $fichier="/dev/ttyUSB0";
-if($ouverture =fopen($fichier, 'a+'))
+$fichierPort = "/dev/ttyUSB0";
+$port = fopen($fichierPort, 'a+');
+if(!$port)
 {
-
-
-
-
-
-        $recuperation="$"."F1".'.'.$ligne['F1'].";".''."F2".'.'.$ligne['F2'].";".''."F3".'.'.$ligne['F3'].";".''."F4".'.'.$ligne['F4'].";".''."F5".'.'.$ligne['F5'].";".''."FP1".'.'.$ligne['FP1'].";".''."FP2".'.'.$ligne['FP2'].";".''."D".'.'.$ligne['D'].'*'."\n";
-
-        fputs($ouverture, $recuperation);
-
-        fclose($ouverture);
+   exit("Impossible d'ouvrir le port $fichierPort");
 }
 
-else echo 'error'.'<br>';
+$mysqli = mysqli_connect("localhost", "celian", "password", "FEUX");
+if (!$mysqli)
+{
+    exit("Erreur de connexion à la base de données FEUX !");
+}
 
-                }
-mysqli_close($connexion);
+$requete = "SELECT * FROM CLASSIQUE";
+$resultat = mysqli_query($mysqli, $requete);
+while ($enregistrement = mysqli_fetch_assoc($resultat))
+{
+    $trame = "$"."F1".'.'.$enregistrement['F1'].";".''."F2".'.'.$enregistrement['F2'].";".''."F3".'.'.$enregistrement['F3'].";".''."F4".'.'.$enregistrement['F4'].";".''."F5".'.'.$enregistrement['F5'].";".''."FP1".'.'.$enregistrement['FP1'].";".''."FP2".'.'.$enregistrement['FP2'].";".''."D".'.'.$enregistrement['D'].'*'."\n";
+    fputs($port, $trame);
+}
+
+$mysqli->close();
+
+fclose($port);
 ?>
-
